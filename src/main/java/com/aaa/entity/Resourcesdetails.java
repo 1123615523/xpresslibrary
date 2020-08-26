22554e983290d4e15ec1af114698da4431f50b6b
wangdengke <1123615523@qq.com>
2020-08-26 20:26:47 +0800

    上传文件777

---
+++ src/main/java/com/aaa/entity/Resourcesdetails.java
@@ -0,0 +1,20 @@
+package com.aaa.entity;
+
+import lombok.Data;
+
+/**
+ * (Resourcesdetails)实体类
+ *
+ * @author makejava
+ * @since 2020-08-26 15:36:27
+ */
+@Data
+public class Resourcesdetails{
+    
+    private Integer did;
+    
+    private String dname;
+    
+    private Integer resourcesid;
+
+}
